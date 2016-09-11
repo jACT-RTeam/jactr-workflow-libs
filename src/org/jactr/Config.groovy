08de328ae98aa790cc81f3087d61f6afad6d860f
Sebastian Lohmeier <sebastian@monochromata.de>
2016-09-11 19:16:26 +0100

    Replace tabs with spaces

--- src/org/jactr/Config.groovy
+++ src/org/jactr/Config.groovy
@@ -4,114 +4,114 @@ package org.jactr;
  * A configuration for a {@link Build}.
  */
 class Config implements Serializable {
-
+    
     /**
      * The label of the Jenkins node to be used to execute the job run
      * from this configuration.
      */
-	public final String labelForJenkinsNode
-
-	/**
-  	 * The URL referring to a maven-metadata.xml file of a Maven repository that each successful build
-  	 * using this configuration deploys to.
-	 */
-	public final String releaseMetaDataURL
-	
-	/**
-	 * The Maven property that will be set with the next version in Eclipse format.
-	 */
-	public final String propertyForEclipseVersion
-	
-	/**
+    public final String labelForJenkinsNode
+    
+    /**
+     * The URL referring to a maven-metadata.xml file of a Maven repository that each successful build
+     * using this configuration deploys to.
+     */
+    public final String releaseMetaDataURL
+    
+    /**
+     * The Maven property that will be set with the next version in Eclipse format.
+     */
+    public final String propertyForEclipseVersion
+    
+    /**
      * The URL referring to the Git repository that will be checked out to base the build on.
      */
-	public final String gitRepoURL
-	
-	/**
-	 * The credentials required to access the Git repository. {@code null}, if the Git repository is public
-	 * and does not require credentials.
-	 */
-	public final String gitCredentialsId
-	
-	/**
-	 * True, if this build uses <a href="https://eclipse.org/tycho/sitedocs/tycho-release/">Tycho</a> to build
-	 * e.g. Eclipse plug-ins.
-	 */
-	public final boolean isTychoBuild
-	
-	/**
-	 * If set, a <a href="http://linux.die.net/man/1/xvfb">Xvfb</a> and a
-	 * <a href="http://www.nongnu.org/ratpoison/">ratpoison window manager</a> shall be created for the display
-	 * {@code :<displayNumber>}.
-	 */
-	public final int displayNumber
-	
+    public final String gitRepoURL
+    
+    /**
+     * The credentials required to access the Git repository. {@code null}, if the Git repository is public
+     * and does not require credentials.
+     */
+    public final String gitCredentialsId
+    
+    /**
+     * True, if this build uses <a href="https://eclipse.org/tycho/sitedocs/tycho-release/">Tycho</a> to build
+     * e.g. Eclipse plug-ins.
+     */
+    public final boolean isTychoBuild
+    
+    /**
+     * If set, a <a href="http://linux.die.net/man/1/xvfb">Xvfb</a> and a
+     * <a href="http://www.nongnu.org/ratpoison/">ratpoison window manager</a> shall be created for the display
+     * {@code :<displayNumber>}.
+     */
+    public final int displayNumber
+    
     /**
      * If set, the jobs named in this list will be triggered if the configured job completes successfully.
      * Triggering will include job parameters to have these jobs update their dependencies to cover the new
      * version created by the configured job.
      */
-	public List<String> jobsToUpdateToNewlyBuiltVersion
-	
+    public List<String> jobsToUpdateToNewlyBuiltVersion
+    
     /**
      * The Maven groupId obtained from {@link #releaseMetaDataURL} before the configuration is build.
      *
      * @see #parseMavenMetadata()
      * @see #build()
      */
-	public final String mavenGroupId
-	
+    public final String mavenGroupId
+    
     /**
      * The Maven artifactId obtained from {@link #releaseMetaDataURL} before the configuration is build.
      *
      * @see #parseMavenMetadata()
      * @see #build()
      */
-	public final String mavenArtifactId
-	
+    public final String mavenArtifactId
+    
     /**
      * The last released Maven version obtained from {@link #releaseMetaDataURL} before the configuration is build.
      *
      * @see #parseMavenMetadata()
      * @see #build()
      */
-	public final String mavenCurrentReleaseVersion
+    public final String mavenCurrentReleaseVersion
 	
-	/**
-	 * Creates a new build configuration for a public Git repository.
-	 * @param releaseMetaDataURL A URL referring to a maven-metadata.xml file of a Maven repository
-	 *							 that each successful build using this configuration deploys to. The meta-data
-	 *							 will be used to obtain the last version number in order to increment it when
-	 *							 starting a new build (see {@link Build#getNextVersion()}).
-	 * @param propertyForEclipseVersion The Maven property that will be set with the next version in Eclipse format.
-	 * @param gitRepoURL A URL referring to the Git repository that will be checked out to base the build on.
-	 * @deprecated Use {@link ConfigBuilder} instead.
-	 */
-	 @Deprecated
-	public Config(String releaseMetaDataURL,
-	              String propertyForEclipseVersion,
-	              String gitRepoURL) {
-	      this(releaseMetaDataURL, propertyForEclipseVersion, gitRepoURL, null, false)
-  	}
+    /**
+     * Creates a new build configuration for a public Git repository.
+     * @param releaseMetaDataURL A URL referring to a maven-metadata.xml file of a Maven repository
+     *                           that each successful build using this configuration deploys to. The meta-data
+     *                           will be used to obtain the last version number in order to increment it when
+     *	                         starting a new build (see {@link Build#getNextVersion()}).
+     * @param propertyForEclipseVersion The Maven property that will be set with the next version in Eclipse format.
+     * @param gitRepoURL A URL referring to the Git repository that will be checked out to base the build on.
+     * @deprecated Use {@link ConfigBuilder} instead.
+     */
+     @Deprecated
+    public Config(String releaseMetaDataURL,
+                  String propertyForEclipseVersion,
+                  String gitRepoURL) {
+        this(releaseMetaDataURL, propertyForEclipseVersion, gitRepoURL, null, false)
+    }
   	
-	/**
-	 * Creates a new build configuration for a private Git repository that requires authentication.
-	 * @param releaseMetaDataURL A URL referring to a maven-metadata.xml file of a Maven repository
-	 *							 that each successful build using this configuration deploys to. The meta-data
-	 *							 will be used to obtain the last version number in order to increment it when
-	 *							 starting a new build (see {@link Build#getNextVersion()}).
-	 * @param propertyForEclipseVersion The Maven property that will be set with the next version in Eclipse format.
-	 * @param gitRepoURL A URL referring to the Git repository that will be checked out to base the build on.
-	 * @param gitCredentialsId The ID of the Jenkins-managed credentials required to access the repository. 
-	 * @deprecated Use {@link ConfigBuilder} instead.
-	 */
-	 @Deprecated
-	public Config(String releaseMetaDataURL,
-	              String propertyForEclipseVersion,
-	              String gitRepoURL,
-	              String gitCredentialsId) {
-	      this(releaseMetaDataURL, propertyForEclipseVersion, gitRepoURL, gitCredentialsId, false)
-  	}
+    /**
+     * Creates a new build configuration for a private Git repository that requires authentication.
+     * @param releaseMetaDataURL A URL referring to a maven-metadata.xml file of a Maven repository
+     *                           that each successful build using this configuration deploys to. The meta-data
+     *                           will be used to obtain the last version number in order to increment it when
+     *                           starting a new build (see {@link Build#getNextVersion()}).
+     * @param propertyForEclipseVersion The Maven property that will be set with the next version in Eclipse format.
+     * @param gitRepoURL A URL referring to the Git repository that will be checked out to base the build on.
+     * @param gitCredentialsId The ID of the Jenkins-managed credentials required to access the repository. 
+     * @deprecated Use {@link ConfigBuilder} instead.
+     */
+     @Deprecated
+    public Config(String releaseMetaDataURL,
+                  String propertyForEclipseVersion,
+                  String gitRepoURL,
+                  String gitCredentialsId) {
+        this(releaseMetaDataURL, propertyForEclipseVersion, gitRepoURL, gitCredentialsId, false)
+    }
   	
 	/**
 	 * Creates a new build configuration for a private Git repository that requires authentication.
@@ -139,10 +139,10 @@ class Config implements Serializable {
 		     isTychoBuild, displayNumber, "2gb")
   	}
   	
-  	/**
-  	 * This constructor shall only be used by {@link ConfigBuilder}.
-  	 */
-  	Config(String releaseMetaDataURL,
+    /**
+     * This constructor shall only be used by {@link ConfigBuilder}.
+     */
+    Config(String releaseMetaDataURL,
            String propertyForEclipseVersion,
            String gitRepoURL,
            String gitCredentialsId,
@@ -153,17 +153,17 @@ class Config implements Serializable {
            String mavenGroupId,
            String mavenArtifactId,
            String mavenCurrentReleaseVersion) {  	
-		this.releaseMetaDataURL = releaseMetaDataURL
-		this.propertyForEclipseVersion = propertyForEclipseVersion
-		this.gitRepoURL = gitRepoURL
-		this.gitCredentialsId = gitCredentialsId
-		this.isTychoBuild = isTychoBuild
-		this.displayNumber = displayNumber
-		this.labelForJenkinsNode = labelForJenkinsNode
-		this.jobsToUpdateToNewlyBuiltVersion = jobsToUpdateToNewlyBuiltVersion
-		this.mavenGroupId = mavenGroupId
-		this.mavenArtifactId = mavenArtifactId
-		this.mavenCurrentReleaseVersion = mavenCurrentReleaseVersion
-  	}
+        this.releaseMetaDataURL = releaseMetaDataURL
+        this.propertyForEclipseVersion = propertyForEclipseVersion
+        this.gitRepoURL = gitRepoURL
+        this.gitCredentialsId = gitCredentialsId
+        this.isTychoBuild = isTychoBuild
+        this.displayNumber = displayNumber
+        this.labelForJenkinsNode = labelForJenkinsNode
+        this.jobsToUpdateToNewlyBuiltVersion = jobsToUpdateToNewlyBuiltVersion
+        this.mavenGroupId = mavenGroupId
+        this.mavenArtifactId = mavenArtifactId
+        this.mavenCurrentReleaseVersion = mavenCurrentReleaseVersion
+    }
   	
 }
--- src/org/jactr/ConfigBuilder.groovy
+++ src/org/jactr/ConfigBuilder.groovy
@@ -6,181 +6,181 @@ package org.jactr;
  */
 public class ConfigBuilder {
 	
-	/**
-  	 * The URL referring to a maven-metadata.xml file of a Maven repository that each successful build
-  	 * using the configured job deploys to.
-	 */
-	private final String releaseMetaDataURL
-	
-	/**
+    /**
+     * The URL referring to a maven-metadata.xml file of a Maven repository that each successful build
+     * using the configured job deploys to.
+     */
+    private final String releaseMetaDataURL
+    
+    /**
      * The URL referring to the Git repository that will be checked out to base the build on.
      */
-	private final String gitRepoURL
-	
-	/**
-	 * Optional credentials required to access the Git repository in case it is not public.
-	 * {@code null}, if the Git repository is public and does not require credentials. Defaults to {@code null}.
-	 */
-	private String gitCredentialsId
-	
+    private final String gitRepoURL
+    
+    /**
+     * Optional credentials required to access the Git repository in case it is not public.
+     * {@code null}, if the Git repository is public and does not require credentials. Defaults to {@code null}.
+     */
+    private String gitCredentialsId
+    
     /**
      * The label of the Jenkins node to be used to execute the job run
      * from the configuration created by the builder. Has a default value.
      */
-	private labelForJenkinsNode = "2gb";
-	
-	/**
-	 * The Maven property that will be set with the next version in Eclipse format. Has a default value.
-	 */
-	private String propertyForEclipseVersion = "newVersionForEclipseIsNotUsed"
-	
-	/**
-	 * True, if the configured build uses <a href="https://eclipse.org/tycho/sitedocs/tycho-release/">Tycho</a> to build
-	 * e.g. Eclipse plug-ins. Defaults to {@code false}.
-	 */
-	private boolean isTychoBuild = false
-	
-	/**
-	 * If set, a <a href="http://linux.die.net/man/1/xvfb">Xvfb</a> and a
-	 * <a href="http://www.nongnu.org/ratpoison/">ratpoison window manager</a> shall be created for the display
-	 * {@code :<displayNumber>}. Defaults to {@code null}.
-	 */
-	private Integer displayNumber = null
-
+    private labelForJenkinsNode = "2gb";
+    
+    /**
+     * The Maven property that will be set with the next version in Eclipse format. Has a default value.
+     */
+    private String propertyForEclipseVersion = "newVersionForEclipseIsNotUsed"
+    
+    /**
+     * True, if the configured build uses <a href="https://eclipse.org/tycho/sitedocs/tycho-release/">Tycho</a> to build
+     * e.g. Eclipse plug-ins. Defaults to {@code false}.
+     */
+    private boolean isTychoBuild = false
+    
+    /**
+     * If set, a <a href="http://linux.die.net/man/1/xvfb">Xvfb</a> and a
+     * <a href="http://www.nongnu.org/ratpoison/">ratpoison window manager</a> shall be created for the display
+     * {@code :<displayNumber>}. Defaults to {@code null}.
+     */
+    private Integer displayNumber = null
+    
     /**
      * If set, the jobs named in this list will be triggered if the configured job completes successfully.
      * Triggering will include job parameters to have these jobs update their dependencies to cover the new
      * version created by the configured job. Defaults to {@code null}.
      */
-	private List<String> jobsToUpdateToNewlyBuiltVersion = null
-
+    private List<String> jobsToUpdateToNewlyBuiltVersion = null
+    
     /**
      * The Maven groupId obtained from {@link #releaseMetaDataURL} before the configuration is build.
      *
      * @see #parseMavenMetadata()
      * @see #build()
      */
-	private String mavenGroupId = null
-	
+    private String mavenGroupId = null
+    
     /**
      * The Maven artifactId obtained from {@link #releaseMetaDataURL} before the configuration is build.
      *
      * @see #parseMavenMetadata()
      * @see #build()
      */
-	private String mavenArtifactId = null
-	
+    private String mavenArtifactId = null
+    
     /**
      * The Maven last released version obtained from {@link #releaseMetaDataURL} before the configuration is build.
      *
      * @see #parseMavenMetadata()
      * @see #build()
      */
-	private String mavenCurrentReleaseVersion = null
+    private String mavenCurrentReleaseVersion = null
 
     /**
      * Create a builder and supply the required configuration information to build
      * from a public Git repository.
      * 
-	 * @param releaseMetaDataURL A URL referring to a maven-metadata.xml file of a Maven repository
-	 *							 that each successful build using the configured job deploys to. The meta-data
-	 *							 will be used to obtain the last version number in order to increment it when
-	 *							 starting a new build (see {@link Build#getNextVersion()}).
+     * @param releaseMetaDataURL A URL referring to a maven-metadata.xml file of a Maven repository
+     *                           that each successful build using the configured job deploys to. The meta-data
+     *                           will be used to obtain the last version number in order to increment it when
+     *                           starting a new build (see {@link Build#getNextVersion()}).
      * @param gitRepoURL A URL referring to the Git repository that will be checked out to base the build on.
      */
-	public ConfigBuilder(String releaseMetaDataURL,
-						 String gitRepoURL) {
-		this.releaseMetaDataURL = releaseMetaDataURL
-		this.gitRepoURL = gitRepoURL
-	}
-	
-	/**
-	 * Configure a custom label to be used to choose the node to run the configured job on.
-	 */
-	public ConfigBuilder useJenkinsNodeWithLabel(String label) {
-		this.labelForJenkinsNode = label
-	}
-	
-	/**
-	 * Set the ID of the Jenkins-managed credentials to be used to access a Git repository that
-	 * requires credentials.
-	 */
-	public ConfigBuilder gitCredentialsId(String credentialsId) {
-		this.gitCredentialsId = credentialsId
-	}
+    public ConfigBuilder(String releaseMetaDataURL,
+    					 String gitRepoURL) {
+        this.releaseMetaDataURL = releaseMetaDataURL
+        this.gitRepoURL = gitRepoURL
+    }
 	
     /**
-	 * Sets the Maven property that will be set with the version (in Eclipse format) that will
-	 * be build by the configured job.
-	 */
-	public ConfigBuilder propertyForEclipseVersion(String propertyName) {
-		this.propertyForEclipseVersion = propertyName
-	}
+     * Configure a custom label to be used to choose the node to run the configured job on.
+     */
+    public ConfigBuilder useJenkinsNodeWithLabel(String label) {
+        this.labelForJenkinsNode = label
+    }
 	
-	/**
-	 * Configures the build to be <a href="https://eclipse.org/tycho/sitedocs/tycho-release/">Tycho</a>-specific.
-	 */
-	public ConfigBuilder isTychoBuild() {
-		this.isTychoBuild = true
-	}
+    /**
+     * Set the ID of the Jenkins-managed credentials to be used to access a Git repository that
+     * requires credentials.
+     */
+    public ConfigBuilder gitCredentialsId(String credentialsId) {
+        this.gitCredentialsId = credentialsId
+    }
 	
-	/**
-	 * Configures the build to have a <a href="http://linux.die.net/man/1/xvfb">Xvfb</a> and a
-	 * <a href="http://www.nongnu.org/ratpoison/">ratpoison window manager</a> for the display
-	 * {@code :<displayNumber>}.
-	 */
-	public ConfigBuilder provideDisplayAndWindowManager(int displayNumber) {
-		this.displayNumber = displayNumber
-	}
+    /**
+     * Sets the Maven property that will be set with the version (in Eclipse format) that will
+     * be build by the configured job.
+     */
+    public ConfigBuilder propertyForEclipseVersion(String propertyName) {
+        this.propertyForEclipseVersion = propertyName
+    }
+	
+    /**
+     * Configures the build to be <a href="https://eclipse.org/tycho/sitedocs/tycho-release/">Tycho</a>-specific.
+     */
+    public ConfigBuilder isTychoBuild() {
+        this.isTychoBuild = true
+    }
+	
+    /**
+     * Configures the build to have a <a href="http://linux.die.net/man/1/xvfb">Xvfb</a> and a
+     * <a href="http://www.nongnu.org/ratpoison/">ratpoison window manager</a> for the display
+     * {@code :<displayNumber>}.
+     */
+    public ConfigBuilder provideDisplayAndWindowManager(int displayNumber) {
+        this.displayNumber = displayNumber
+    }
 
     /**
      * Set a list of job names that will be triggered if the configured job completes successfully.
      * Triggering will include job parameters to have these jobs update their dependencies to cover the new
      * version created by the configured job.
      */
-	public ConfigBuilder updateDependentJobsToNewlyBuiltVersion(String... jobNames) {
-		this.jobsToUpdateToNewlyBuiltVersion = java.util.Arrays.asList(jobNames)
-	}
+    public ConfigBuilder updateDependentJobsToNewlyBuiltVersion(String... jobNames) {
+        this.jobsToUpdateToNewlyBuiltVersion = java.util.Arrays.asList(jobNames)
+    }
 
     /**
      * Create a new configuration from the information supplied to the builder.
      */
-	public Config build() {
-		parseMavenMetadata()
-	    return new Config(
-	    	this.releaseMetaDataURL,
-	    	this.propertyForEclipseVersion,
-	    	this.gitRepoURL,
-	    	this.gitCredentialsId,
-	    	this.isTychoBuild,
-	    	this.displayNumber,
-	    	this.labelForJenkinsNode,
-	    	this.jobsToUpdateToNewlyBuiltVersion,
-	    	this.mavenGroupId,
-	    	this.mavenArtifactId,
-	    	this.mavenCurrentReleaseVersion)
-	}
+    public Config build() {
+        parseMavenMetadata()
+        return new Config(
+            this.releaseMetaDataURL,
+            this.propertyForEclipseVersion,
+            this.gitRepoURL,
+            this.gitCredentialsId,
+            this.isTychoBuild,
+            this.displayNumber,
+            this.labelForJenkinsNode,
+            this.jobsToUpdateToNewlyBuiltVersion,
+            this.mavenGroupId,
+            this.mavenArtifactId,
+            this.mavenCurrentReleaseVersion)
+    }
 	
-  	private void parseMavenMetadata() {
-		node(this.labelForJenkinsNode) {
-			def tmpDir=pwd tmp: true
-		
-			// Get the Maven meta-data: groupId, artifactId, release version
-			def mavenMetaDataFile = tmpDir+'/maven-metadata.xml'
-			def groupIdFile = tmpDir+'/maven.groupId'
-			def artifactIdFile = tmpDir+'/maven.artifactId'
-			def versionFile = tmpDir+'/maven.release'
-			sh 'curl --silent '+config.releaseMetaDataURL+' > '+mavenMetaDataFile
-			sh 'xpath -e metadata/groupId -q '+mavenMetaDataFile+' | sed --regexp-extended "s/<\\/?groupId>//g" > '+groupIdFile
-			sh 'xpath -e metadata/artifactId -q '+mavenMetaDataFile+' | sed --regexp-extended "s/<\\/?artifactId>//g" > '+artifactIdFile
-			sh 'xpath -e metadata/versioning/release -q '+mavenMetaDataFile+' | sed --regexp-extended "s/<\\/?release>//g" > '+versionFile
-			this.mavenGroupId = readFile(groupIdFile).trim()
-			this.mavenArtifactId = readFile(artifactIdFile).trim()
-			this.mavenCurrentReleaseVersion = readFile(versionFile).trim()
-			sh 'rm '+mavenMetaDataFile
-			sh 'rm '+groupIdFile
-			sh 'rm '+artifactIdFile
-			sh 'rm '+versionFile
-		}
-  	}
+    private void parseMavenMetadata() {
+        node(this.labelForJenkinsNode) {
+            def tmpDir=pwd tmp: true
+            
+            // Get the Maven meta-data: groupId, artifactId, release version
+            def mavenMetaDataFile = tmpDir+'/maven-metadata.xml'
+            def groupIdFile = tmpDir+'/maven.groupId'
+            def artifactIdFile = tmpDir+'/maven.artifactId'
+            def versionFile = tmpDir+'/maven.release'
+            sh 'curl --silent '+config.releaseMetaDataURL+' > '+mavenMetaDataFile
+            sh 'xpath -e metadata/groupId -q '+mavenMetaDataFile+' | sed --regexp-extended "s/<\\/?groupId>//g" > '+groupIdFile
+            sh 'xpath -e metadata/artifactId -q '+mavenMetaDataFile+' | sed --regexp-extended "s/<\\/?artifactId>//g" > '+artifactIdFile
+            sh 'xpath -e metadata/versioning/release -q '+mavenMetaDataFile+' | sed --regexp-extended "s/<\\/?release>//g" > '+versionFile
+            this.mavenGroupId = readFile(groupIdFile).trim()
+            this.mavenArtifactId = readFile(artifactIdFile).trim()
+            this.mavenCurrentReleaseVersion = readFile(versionFile).trim()
+            sh 'rm '+mavenMetaDataFile
+            sh 'rm '+groupIdFile
+            sh 'rm '+artifactIdFile
+            sh 'rm '+versionFile
+        }
+    }
 }
