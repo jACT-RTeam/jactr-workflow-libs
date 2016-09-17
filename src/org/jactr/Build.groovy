a3bf0ef88a5088401d8b3570ac55632f11ba3ee7
Sebastian Lohmeier <sebastian@monochromata.de>
2016-09-17 22:28:11 +0100

    #5

--- src/org/jactr/Build.groovy
+++ src/org/jactr/Build.groovy
@@ -52,7 +52,7 @@ def run(Config config) {
                     // Ensure the repository has been cloned, checkout the file to be modified
                     sh """cd """+tmpDir+""" \
                             && echo "Foo" \
-                            && if [ ! -e """+dependencyUpdate.gitRepoName+""" ]; then 
+                            && if [ ! -e """+dependencyUpdate.gitRepoName+""" ] then 
                                 echo "Hello world"
                                fi \
                             && cd """+dependencyUpdate.gitRepoName+""" \
