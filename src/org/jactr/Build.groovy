af4a2b3a2aa422bf66530ded8686ec6a40c4a088
Sebastian Lohmeier <sebastian@monochromata.de>
2016-09-17 22:26:48 +0100

    #5

--- src/org/jactr/Build.groovy
+++ src/org/jactr/Build.groovy
@@ -51,7 +51,7 @@ def run(Config config) {
                                  [$class: 'FileBinding', credentialsId: dependencyUpdate.gitFileCredentialsId, variable: 'GIT_CREDENTIALS_FILE']]) {
                     // Ensure the repository has been cloned, checkout the file to be modified
                     sh """cd """+tmpDir+""" \
-                            && echo "Foo"
+                            && echo "Foo" \
                             && if [ ! -e """+dependencyUpdate.gitRepoName+""" ]; then 
                                 echo "Hello world"
                                fi \
