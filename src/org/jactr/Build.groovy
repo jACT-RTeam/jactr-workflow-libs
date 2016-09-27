6aa1c8375a4bbe268c20b9972c2c658fc3556beb
Sebastian Lohmeier <sebastian@monochromata.de>
2016-09-27 21:36:43 +0200

    Change commit message for dependency update

--- src/org/jactr/Build.groovy
+++ src/org/jactr/Build.groovy
@@ -43,7 +43,7 @@ def run(Config config) {
                         
                     // Push the change
                     gitPush(config, config.dependencyUpdate.modifiedFilesPattern,
-                        'Bump version of dependency '+dependencyToUpdateForMaven+' to '+config.script.newDependencyVersion+' in '+config.dependencyUpdate.modifiedFilesPattern)
+                        'Update dependency '+dependencyToUpdateForMaven+' to '+config.script.newDependencyVersion+' in '+config.dependencyUpdate.modifiedFilesPattern)
                 }
            }
 		   
